ec1ec1ee95e70d0e29db6b396c3bcc3bc3bd5528
macbook <finonty21@yahoo.fr>
2023-12-22 17:48:07 +0100

    Initialisation project

---
+++ OriginalyRecipe/src/main/java/org/recipe/originalyrecipe/OriginalyRecipeApplication.java
@@ -0,0 +1,13 @@
+package org.recipe.originalyrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginalyRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginalyRecipeApplication.class, args);
+	}
+
+}
---
+++ OriginalyRecipe/src/test/java/org/recipe/originalyrecipe/OriginalyRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.recipe.originalyrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalyRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
